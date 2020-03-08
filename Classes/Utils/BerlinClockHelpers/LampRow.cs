76821fbc44477b55f9a1453b3c00032577c9984e
Otávio Larrosa <oalarrosa@stefanini.com>
2020-03-08 16:27:36 -0300

    Refactored LampRow Helper

--- Classes/Utils/BerlinClockHelpers/LampRow.cs
+++ Classes/Utils/BerlinClockHelpers/LampRow.cs
@@ -1,4 +1,5 @@
 ﻿using BerlinClock.Classes.Utils.ExtensionMethods;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace BerlinClock.Classes.Utils.BerlinClockHelpers
@@ -7,15 +8,26 @@ namespace BerlinClock.Classes.Utils.BerlinClockHelpers
     {
         internal static string GetLampRow(int totalNumberLamps, int numberLampsOn, StatusLampEnum statusLamp)
         {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder result = new StringBuilder();
+            result.Append(AddLampsOn(numberLampsOn, statusLamp));
+            result.Append(AddLampsOff(numberLampsOn, totalNumberLamps));
+            return result.ToString();
+        }
 
-            for (int i = 0; i < numberLampsOn; i++)
-                sb.Append(statusLamp.GetDescription());
+        private static string AddLampsOn(int numberLampsOn, StatusLampEnum statusLamp)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < numberLampsOn; i++) // For each lamp on, add this to result
+                result.Append(statusLamp.GetDescription());
+            return result.ToString();
+        }
 
-            for (int i = sb.ToString().Length; i < totalNumberLamps; i++)
-                sb.Append("O");
-
-            return sb.ToString();
+        private static string AddLampsOff(int numberLampsOn, int totalNumberLamps)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = numberLampsOn; i < totalNumberLamps; i++) // for each lamp off, add this to result, until the end of the lamps in that row.
+                result.Append(StatusLampEnum.OFF.GetDescription());
+            return result.ToString();
         }
     }
 }
